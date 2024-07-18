acc115d469ba53db85cf6ce8417129e4e5282207
Kirill Zlobin <gungstarbeiter@gmail.com>
2024-07-18 15:22:56 +0500

    Update alert.sh

--- alert.sh
+++ alert.sh
@@ -16,11 +16,7 @@ if [ -n "$SSH_CLIENT" ]; then
 
 	IPINFO="https://ipinfo.io/${CLIENT_IP}"
 
-	TEXT="<b>${SERVERNAME}</b>
-	Connection from ${CLIENT_IP} as ${USER} to ${SRV_IP}
-	Date: ${DATE}
-	TimeZone:${TZ}
-	More informations: ${IPINFO}"
+	TEXT="❗️<b>${SERVERNAME}</b>%0A%0A<u>Connection from</u> <b>${CLIENT_IP}</b>as <b>${USER}</b>%0A<u>Time:</u> ${DATE}%0ATimeZone:${TZ}%0A%0A<u>More information:</u> ${IPINFO}"
 
         curl -s -d "chat_id=$i&text=${TEXT}&disable_web_page_preview=true&parse_mode=html" $URL > /dev/null
 fi
